89eeb61179c55acfe0f4d1669ad1065694c382e6
buiminhquan17 <hellobuiminhquan@gmail.com>
2025-07-15 14:32:26 +0700

    Simple Pygame program

--- game.py
+++ game.py
@@ -1,5 +1,31 @@
 import pygame
-import sys
 
 
-print("Hello, Pygame!")
+def main() -> None:
+    pygame.init()
+
+    SCREEN_WIDTH: int = 500
+    SCREEN_HEIGHT: int = 500
+    BLACK_COLOR: list[int] = [0, 0, 0]
+
+    # Set up the window
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+    running: bool = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(BLACK_COLOR)
+
+        pygame.draw.circle(screen, [0, 255, 0], [250, 250], 75)
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
